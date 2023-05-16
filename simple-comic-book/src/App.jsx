e374ea39a35753c6fdcf0c6f455c03e901c575f0
Alexander Lind <alexander_lind95@hotmail.com>
2023-05-16 15:05:25 +0200

    new

---
+++ simple-comic-book/src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+
+function App() {
+
+  return (
+ <div className="wrapper">
+  <div className="header">
+    <p>Comic book </p>
+ <img src="/batmanComicLogo.png" alt="batmanLogo" />
+  </div>
+  <div className="picture1">
+  <p class="top-left">Suddenly...</p>
+  </div>
+  <div className="box1">
+  <p class="top-left-mobile">Batman strikes against Joker</p>
+  <p class="bottom-right-mobile">Batman strikes against Joker</p>
+  </div>
+  <div className="box2">
+  </div>
+  <div className="box3">
+  <div class="speech-bubble">
+   <p>Aarghh...</p>
+</div>
+
+  </div>
+  <div className="box4">
+  <p class="top">
+But out of nowhere, Joker manages to strike back and escape.</p>
+  </div>
+  <div className="box5">
+  <p class="bottom-left">
+To be continued..</p>
+  </div>
+  <div className="picture2"></div>
+  <div className="footer">
+    <p>Made by Alexander Lind</p>
+    <p>CSS-Grid</p>
+  </div>
+  </div>
+  )
+}
+
+export default App;
